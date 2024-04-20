14ecb635293652b3a965dda5e4b8bcc8315f8b47
Tamal Saha <tamal@appscode.com>
2024-04-20 13:58:11 -0700

    wip
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
+	fmt.Println("listening on :80")
 	http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
 	}))
